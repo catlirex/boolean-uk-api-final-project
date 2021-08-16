b5e8c924144524fdb90abc8e7e412c10633b997f
Catherine Li <catherine.uk.20@gmail.com>
2021-08-16 12:05:20 +0100

    first commit

---
+++ backend_app/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const path = require("path");
+
+const logger = require("morgan");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+
+module.exports = app;
