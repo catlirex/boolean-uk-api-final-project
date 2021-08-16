8954504bc9f3eeb1c9da7ff2ddc03c3d3f7e0120
Catherine Li <catherine.uk.20@gmail.com>
2021-08-16 22:43:49 +0100

    update dbClient name

--- backend_app/src/resources/specialRequests/controller.js
+++ backend_app/src/resources/specialRequests/controller.js
@@ -2,7 +2,7 @@ const { special_request } = require("../../utils/database");
 
 const getAll = async (req, res) => {
   try {
-    const allSpecialRequests = await special_request.findMany();
+    const allSpecialRequests = await specialRequest.findMany();
     res.json(allSpecialRequests);
   } catch (error) {
     res.json({ error: error.message });
