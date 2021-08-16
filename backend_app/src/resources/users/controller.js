052362c4cb55517a5e96ad04bb82aa03e7cbec6b
Anas Ansari <anas_ansar1@outlook.com>
2021-08-16 21:24:54 +0100

    created user routers

--- backend_app/src/resources/users/controller.js
+++ backend_app/src/resources/users/controller.js
@@ -1,1 +1,43 @@
-// const { user } = require("../../utils/database");
+const { user } = require("../../utils/database");
+
+const getAllUsers = async (req, res) => {
+  try {
+    const allUsers = await user.findMany();
+
+    res.json(allUsers);
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+};
+
+const getUserById = async (req, res) => {
+  const id = parseInt(req.params.id);
+
+  try {
+    const aUser = await user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    res.json(aUser);
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+};
+
+const createUser = async (req, res) => {
+  const newUser = req.body;
+
+  try {
+    const createdUser = await user.create({
+      data: newUser,
+    });
+
+    res.json(createUser);
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+};
+
+module.exports = { getAllUsers, getUserById, createUser };
