a9fb6e8b5a93852319a365e008b1090085a20f42
Catherine Li <catherine.uk.20@gmail.com>
2021-08-17 18:36:06 +0100

    create app container fixed size and add BG image

--- frontend_app/src/App.tsx
+++ frontend_app/src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello there</h1>
+    <div className="background-image">
+      <div className="App">
+        <h1>Hello there</h1>
+      </div>
     </div>
   );
 }
