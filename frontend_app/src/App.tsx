ed6f5c4423974187f9f7da8275c92b1a95c78053
Catherine Li <catherine.uk.20@gmail.com>
2021-08-17 18:53:26 +0100

    app routes install zustand &css library

--- frontend_app/src/App.tsx
+++ frontend_app/src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
@@ -6,6 +7,23 @@ function App() {
     <div className="background-image">
       <div className="App">
         <h1>Hello there</h1>
+        <Switch>
+          <Route path="/" exact>
+            {/* <LoginPage/> */}
+          </Route>
+
+          <Route path="/user/:id" exact>
+            {/* <UserPage/> */}
+          </Route>
+
+          <Route path="/shop/:id" exact>
+            {/* <ShopPage/> */}
+          </Route>
+
+          <Route>
+            <h3>Error 404</h3>
+          </Route>
+        </Switch>
       </div>
     </div>
   );
